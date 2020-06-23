65c648b4ab92549cb19d7690dbd1df3fb9a2b21d
Keila <keiladaianefernandes24@gmail.com>
2020-06-23 17:47:49 -0300

    animation character added

--- sketch.js
+++ sketch.js
@@ -1,6 +1,9 @@
-let sceneImage;
-let characterImage;
 let scenario;
+let sceneImage;
+
+let character;
+let characterImage;
+
 let songGame;
 
 function preload() {
@@ -12,42 +15,17 @@ function preload() {
 function setup() {
   createCanvas(windowWidth, windowHeight);
   scenario = new Scenario(sceneImage, 2.5)
+  character = new Character(characterImage)
   songGame.loop()
 }
 
 function draw() {
   scenario.show()
   scenario.move()
-  image(characterImage, 0, height-135, 110, 135, 0, 0, 220, 270)
-  
+
+  character.show()
 }
 
-
-
-class Scenario {
-  constructor(image, velocity) {
-    this.image = image
-    this.velocity = velocity
-    this.x1 = 0
-    this.x2 = width
-  }
-  
-  show() {
-    image(this.image, this.x1, 0, width, height)
-    image(this.image, this.x2, 0, width, height)
-  }
-
-  move() {
-    this.x1 -= this.velocity
-    this.x2 -= this.velocity 
-    if(this.x1 < -width) {
-      this.x1  = width
-    }
-    if(this.x2 < -width) {
-      this.x2  = width
-    }
-  }
-}
 
 
 
